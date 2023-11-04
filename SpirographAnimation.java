67105783717b9b7e079fb8e34d3cfbd402a54cf7
AInan05 <147404730+AInan05@users.noreply.github.com>
2023-11-04 14:59:51 +0000

    Add files via upload

---
+++ SpirographAnimation.java
@@ -0,0 +1,69 @@
+import javax.swing.*;
+import javax.swing.Timer;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.*;
+import java.util.List;
+
+public class SpirographAnimation extends JPanel implements ActionListener {
+    private double t = 0;
+    private Timer timer;
+    private HashMap<Artist, List<Point>> spiroToTrailMap = new HashMap<>();
+
+    public SpirographAnimation() {
+        setSpiro();
+        timer = new Timer(1, this); // 1ms delay for animation
+        timer.start();
+    }
+
+    public void setSpiro() {
+        while (true) {
+            Artist artist = new Artist();
+            if (artist.initiateVectors()) {
+                return;
+            }
+            List<Point> trail = new ArrayList<>();
+            spiroToTrailMap.put(artist, trail);
+        }
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        int centerX = getWidth() / 2;
+        int centerY = getHeight() / 2;
+
+        // Calculate point using a parametric equation (x, y) = (cos(t), sin(t))
+        for (Artist artist : spiroToTrailMap.keySet()) {
+            double[] xyCord = artist.calculate();
+            int x = (int) (centerX + xyCord[0]);
+            int y = (int) (centerY + xyCord[1]);
+            // Add the current position to the trail
+            spiroToTrailMap.get(artist).add(new Point(x, y));
+            // Draw the trail
+            g.setColor(Color.BLACK);
+            for (Point point : spiroToTrailMap.get(artist)) {
+                g.fillOval(point.x - 2, point.y - 2, 4, 4);
+                // Draw the circle at the current position
+                g.setColor(Color.RED);
+                g.fillOval(x - 5, y - 5, 10, 10);
+            }
+        }
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        t += 0.05; // Increment the parameter for animation
+        repaint();
+    }
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Spirographs my beloved");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        SpirographAnimation animation = new SpirographAnimation();
+        frame.add(animation);
+        frame.setSize(400, 400);
+        frame.setVisible(true);
+    }
+}
